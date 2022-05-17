e7f77b48f08b0459512daf377ac867148f7ceef7
maxwelltesla <86696381+maxwelltesla@users.noreply.github.com>
2022-05-17 16:04:31 +0800

    Create base.py

---
+++ base.py
@@ -0,0 +1,20 @@
+class Nothing:
+  __init__(self, remembered, forgotten):
+    self.remembered = remembered
+    self.forgotten = forgotten
+    pass
+  
+  born(self):
+    pass
+  
+  growth(self):
+    pass
+  
+  action(self):
+    pass
+  
+  social(self):
+    pass
+  
+  death(self):
+    pass
