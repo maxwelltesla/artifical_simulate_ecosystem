6b4d23c961d9e65a71f571a21321a65bd112e3fa
maxwelltesla <86696381+maxwelltesla@users.noreply.github.com>
2022-05-17 17:46:25 +0800

    Update base.py

--- base.py
+++ base.py
@@ -5,23 +5,31 @@ class Nothing:
   def beforeborn(self):
     pass
   
-  def born(self):
+  def born(self, pregendtime):
+    this.pregendtime = pregendtime
     pass
   
-  def growth(self):
+  def growth(self，growperiodtime):
+     this.growperiodtime = growperiodtime
     pass
   
   def action(self):
     pass
   
-  def absorb(self):
+  def absorb(self, absorbname, absorbcount):
+    this.absorbname = absorbname
+    this.absorbcount = absorbcount
     pass
   
-  def spreadout(self):
+  def spreadout(self, spreadoutname, spreadoutcount):
+    this.spreadoutname = spreadoutname
+    this.spreadoutcount = spreadoutcount
     pass
   
-  def death(self):
+  def death(self, deltafromborn):
+    this.deltafromborn = deltafromborn
     pass
   
-  def disappear(self):
+  def disappear(self, deltafromdeath):
+    this.deltafromdeath = deltafromdeath
     pass
